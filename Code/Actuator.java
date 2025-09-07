6af5b4da8114bbdb631397cd40f1511f978b77da
Srijak1202 <srijak1202@gmail.com>
2025-09-08 00:10:04 +0530

    Add files via upload
    The Sensor class represents a device that detects or measures changes in the environment. It provides input data to the system for decision-making.
    The Actuator class represents a device that performs an action based on given input or control signals. It acts as the output mechanism.

---
+++ Code/Actuator.java
@@ -0,0 +1,16 @@
+package Team_24_HeartAttack;
+import org.fog.entities.Actuator;
+
+// Actuator Manager creates and manages the actuator (alarm)
+// Sample implementation to create actuator
+
+public class Actuator_Manager
+{
+    
+	// Creating actuator which gives alarms for critical conditions
+    public static Actuator create_actuator()
+    {
+        System.out.println("Creating Acuator : Alarm");
+        return null;
+    }
+}
---
+++ Code/Sensor.java
@@ -0,0 +1,16 @@
+package Team_24_HeartAttack;
+import org.fog.entities.Sensor;
+
+// Sensor Manager creates and manages the sensor (heart_rate_sensor,blood_pressure_sensor,glucose_level_sensor)
+//Sample implementation to create sensor
+
+public class Sensor_Manager
+{
+    
+	// Creating the sensor
+    public static Sensor create_sensor()
+    {
+        System.out.println("Creating Sensor : Healthcare Devices");
+        return null;
+    }
+}

0235960963e7688bfdc0f6b60ed16988711b1b37
Srijak1202 <srijak1202@gmail.com>
2025-09-17 23:08:52 +0530

    Update Actuator.java
    Actuator_Manager for creating emergency alarm actuator in healthcare app, receiving ALARM_SIGNAL with basic logs and error handling

--- Code/Actuator.java
+++ Code/Actuator.java
@@ -1,16 +1,24 @@
 package Team_24_HeartAttack;
+
 import org.fog.entities.Actuator;
 
-// Actuator Manager creates and manages the actuator (alarm)
-// Sample implementation to create actuator
-
-public class Actuator_Manager
-{
+public class Actuator_Manager {
     
-	// Creating actuator which gives alarms for critical conditions
-    public static Actuator create_actuator()
-    {
-        System.out.println("Creating Acuator : Alarm");
-        return null;
+    // Creating actual emergency alarm actuator
+    public static Actuator create_actuator() {
+        System.out.println("Creating Actuator : Emergency Alarm");
+        try {
+            Actuator actuator = new Actuator(
+                "emergency_alarm",    // Actuator name
+                "ACTUATOR",           // Actuator type
+                "HEALTHCARE",         // User ID (application context)
+                "ALARM_SIGNAL"        // Tuple type (what signal it receives)
+            );
+            System.out.println("Actuator created: Emergency Alert System");
+            return actuator;
+        } catch (Exception e) {
+            System.err.println("Error creating actuator: " + e.getMessage());
+            return null;
+        }
     }
 }

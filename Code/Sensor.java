3520339f2c413ee05a79b745d17f04dbf513cbdd
Srijak1202 <srijak1202@gmail.com>
2025-09-17 23:04:44 +0530

    Update Sensor.java
    Sensor_Manager class that creates a healthcare sensor. The sensor sends health data every 5 seconds for the healthcare application. It also has simple logs and error handling to show if the sensor is created successfully.

--- Code/Sensor.java
+++ Code/Sensor.java
@@ -1,16 +1,28 @@
 package Team_24_HeartAttack;
+
 import org.fog.entities.Sensor;
+import org.fog.utils.distribution.DeterministicDistribution;
 
-// Sensor Manager creates and manages the sensor (heart_rate_sensor,blood_pressure_sensor,glucose_level_sensor)
-//Sample implementation to create sensor
-
-public class Sensor_Manager
-{
+public class Sensor_Manager {
     
-	// Creating the sensor
-    public static Sensor create_sensor()
-    {
-        System.out.println("Creating Sensor : Healthcare Devices");
-        return null;
+    // Creating actual healthcare sensor
+    public static Sensor create_sensor() {
+        System.out.println("Creating Healthcare Sensor");
+        try {
+            // Create sensor that generates data every 5 seconds
+            Sensor sensor = new Sensor(
+                "health_sensor",           // Sensor name
+                "SENSOR",                  // Sensor type
+                new DeterministicDistribution(5), // Data generation interval (5 seconds)
+                "HEALTHCARE",              // User ID (application context)
+                "HEALTH_DATA",             // Tuple type (what data it sends)
+                1                          // Sensor ID
+            );
+            System.out.println("Sensor created: Healthcare Monitoring Device");
+            return sensor;
+        } catch (Exception e) {
+            System.err.println("Error creating sensor: " + e.getMessage());
+            return null;
+        }
     }
 }
